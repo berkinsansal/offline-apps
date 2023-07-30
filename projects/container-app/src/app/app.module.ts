d8fb161f1909d266264822e7d1bf5d5c8ddc59db
Berkin Sansal <berkin.sansal@siemens.com>
2023-07-30 16:57:47 +0300

    home component added

--- projects/container-app/src/app/app.module.ts
+++ projects/container-app/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotFoundComponent
+    HomeComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
