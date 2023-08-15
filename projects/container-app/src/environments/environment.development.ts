560cc9983aa41cff4d37225b23f43ee350a68f79
Berkin Sansal <berkin.sansal@siemens.com>
2023-08-16 01:38:55 +0300

    new environment added

--- projects/container-app/src/environments/environment.development.ts
+++ projects/container-app/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    production: false
+    production: false,
+    mfManifestFilePath: 'assets/mf.manifest.development.json'
 };
--- projects/container-app/src/main.ts
+++ projects/container-app/src/main.ts
@@ -1,6 +1,7 @@
 import { loadManifest } from '@angular-architects/module-federation';
+import { environment } from './environments/environment';
 
-loadManifest('assets/mf.manifest.json')
+loadManifest(environment.mfManifestFilePath)
     .catch(err => console.error('Error loading remote entries', err))
     .then(() => import('./bootstrap'))
     .catch(err => console.error(err));
