957828197ad6e77b6e41ef59649d245fcf9027e7
Berkin Sansal <berkin.sansal@siemens.com>
2023-08-16 13:54:31 +0300

    minor changes

--- projects/calculator/src/app/main/main.component.ts
+++ projects/calculator/src/app/main/main.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+    // https://www.codehim.com/html5-css3/javascript-scientific-calculator/
 
 }
